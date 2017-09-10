74d0d88344462c4df3a8d5f72f63523c878353b9
fyyc1204 <kaibinzhao@163.com>
2017-09-10 22:15:13 +0800

    测试

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,6 +3,7 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <el-button type="primary">test</el-button>
+    <span>{{msg}}</span>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
